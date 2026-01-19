582af9f199cc8dd4f35899d8113ee81092a1e3ff
cl-o-lc <sujin2839@gmail.com>
2026-01-20 00:42:16 +0900

    FLYW-108 feat: 유효하지 않은 시간 입력에 대한 스킵 타입 추가 (`INVALID_TIME`) (#49)

--- src/main/java/com/flyway/pricing/PricingSkipReason.java
+++ src/main/java/com/flyway/pricing/PricingSkipReason.java
@@ -3,6 +3,7 @@ package com.flyway.pricing;
 public final class PricingSkipReason {
     public static final String INVALID_SEATS = "INVALID_SEATS";
     public static final String INVALID_PRICE = "INVALID_PRICE";
+    public static final String INVALID_TIME = "INVALID_TIME";
     public static final String COOLDOWN = "COOLDOWN";
     public static final String SMALL_DIFF = "SMALL_DIFF";
 
