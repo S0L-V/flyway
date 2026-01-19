a505fa2931e8d992c8d02b2653bd5416a5a6924a
cl-o-lc <sujin2839@gmail.com>
2026-01-20 00:00:22 +0900

    FLYW-108 feat: 가격 변동 skip 사유 상수 추가 (#49)

---
+++ src/main/java/com/flyway/pricing/PricingSkipReason.java
@@ -0,0 +1,10 @@
+package com.flyway.pricing;
+
+public final class PricingSkipReason {
+    public static final String INVALID_SEATS = "INVALID_SEATS";
+    public static final String INVALID_PRICE = "INVALID_PRICE";
+    public static final String COOLDOWN = "COOLDOWN";
+    public static final String SMALL_DIFF = "SMALL_DIFF";
+
+    private PricingSkipReason() {}
+}
