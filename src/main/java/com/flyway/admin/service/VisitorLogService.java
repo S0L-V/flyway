10988cd7400799439a3f5fc26b8af65bfe6f72ac
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-01-25 20:29:37 +0900

    FLYW-152 feat: VisitorLogService 인터페이스 추가 (#128)

--- src/main/java/com/flyway/admin/service/VisitorLogService.java
+++ src/main/java/com/flyway/admin/service/VisitorLogService.java
@@ -8,4 +8,9 @@ public interface VisitorLogService {
 	 * 방문자 로그를 별도의 트랜잭션에 저장
 	 */
 	void saveVisitorLog(VisitorLog visitorLog);
+
+	/**
+	 * 해당 세션 ID로 오늘 방문한 기록이 있는지 확인
+	 */
+	boolean existsToday(String sessionId);
 }
