78623ac1978dc3ff8b2ca5ad1569787b080f5b14
김민서 <minseokim0113@gmail.com>
2026-01-26 10:35:02 +0900

    FLYW-154 fix: main -> home 변경 (#135)

--- src/main/java/com/flyway/security/config/SecurityConfigWeb.java
+++ src/main/java/com/flyway/security/config/SecurityConfigWeb.java
@@ -31,7 +31,7 @@ public class SecurityConfigWeb extends WebSecurityConfigurerAdapter {
     };
 
     private static final String[] PUBLIC_ENDPOINTS = {
-            "/", "/login", "/loginProc", "/signup", "/admin", "/admin/**", "/auth/**", "/search/**", "/main"
+            "/", "/login", "/loginProc", "/signup", "/admin", "/admin/**", "/auth/**", "/search/**"
     };
 
     private final JwtProvider jwtProvider;
