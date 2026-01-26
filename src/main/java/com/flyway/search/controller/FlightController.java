b511bf56bd7456450ef8bf738f1311ac77d9f58d
김민서 <minseokim0113@gmail.com>
2026-01-26 10:44:33 +0900

    FLYW-154 fix: 코드레빗 리뷰 반영 (#135)

--- src/main/java/com/flyway/search/controller/FlightController.java
+++ src/main/java/com/flyway/search/controller/FlightController.java
@@ -1,7 +1,5 @@
 package com.flyway.search.controller;
 
-import com.flyway.search.service.FlightService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,10 +10,5 @@ public class FlightController {
     @GetMapping("/search")
     public String list() {
         return "search/search";
-    }
-
-    @GetMapping("/main")
-    public String main() {
-        return "main";
     }
 }
