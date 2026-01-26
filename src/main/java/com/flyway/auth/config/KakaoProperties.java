d8f8509ab2798c5c3e159852e667e372d9816f86
gaeunnlee <gaeunnnleee@gmail.com>
2026-01-26 23:05:04 +0900

    FLYW-157 feat: KakaoProperties 설정 클래스 추가 (#144)

---
+++ src/main/java/com/flyway/auth/config/KakaoProperties.java
@@ -0,0 +1,19 @@
+package com.flyway.auth.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+@Component
+@Getter
+@Setter
+public class KakaoProperties {
+    @Value("${kakao.client-id}")
+    private String kakaoClientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String kakaoRedirectUri;
+
+    @Value("${kakao.client-secret}")
+    private String kakaoClientSecret;
+}
