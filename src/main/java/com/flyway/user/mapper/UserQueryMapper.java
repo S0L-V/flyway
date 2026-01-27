e8e274aab91e82f272c6a3872b0a533d4de64802
gaeunnlee <gaeunnnleee@gmail.com>
2026-01-27 15:56:26 +0900

    FLYW-55 feat: 회원 목록, 단건 조회 Repository, Mapper 구현  (#21)

---
+++ src/main/java/com/flyway/user/mapper/UserQueryMapper.java
@@ -0,0 +1,21 @@
+package com.flyway.user.mapper;
+
+import com.flyway.user.dto.UserFullJoinRow;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface UserQueryMapper {
+    /**
+     * 회원 정보 단건 조회
+     */
+    UserFullJoinRow findFullJoinByUserId(@Param("userId") String userId);
+
+    /**
+     * 회원 정보 목록 조회
+     * @param status status 기준 필터링 (null: 전체 조회)
+     */
+    List<UserFullJoinRow> findFullJoinAll(@Param("status") String status);
+}
---
+++ src/main/java/com/flyway/user/repository/UserQueryRepositoryImpl.java
@@ -0,0 +1,29 @@
+// src/main/java/com/flyway/user/repository/UserQueryRepositoryImpl.java
+package com.flyway.user.repository;
+
+import com.flyway.auth.domain.AuthStatus;
+import com.flyway.user.dto.UserFullJoinRow;
+import com.flyway.user.mapper.UserQueryMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserQueryRepositoryImpl implements UserQueryRepository {
+
+    private final UserQueryMapper userQueryMapper;
+
+    @Override
+    public Optional<UserFullJoinRow> findByUserId(String userId) {
+        return Optional.ofNullable(userQueryMapper.findFullJoinByUserId(userId));
+    }
+
+    @Override
+    public List<UserFullJoinRow> findAll(AuthStatus status) {
+        String statusParam = (status == null) ? null : status.name();
+        return userQueryMapper.findFullJoinAll(statusParam);
+    }
+}
