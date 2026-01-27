ddde20d67ecf7a0264d7461f2ad65203c40cdd8a
gaeunnlee <gaeunnnleee@gmail.com>
2026-01-27 22:36:40 +0900

    FLYW-55 feat: 공통 응답 포맷 PageInfo, PageResult 추가 (#21)

---
+++ src/main/java/com/flyway/template/common/PageResult.java
@@ -0,0 +1,16 @@
+package com.flyway.template.common;
+
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public class PageResult<T> {
+    private final List<T> data;
+    private final PageInfo page;
+
+    public PageResult(List<T> data, PageInfo page) {
+        this.data = data;
+        this.page = page;
+    }
+}
