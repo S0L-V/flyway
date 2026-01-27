2c7b3378782aae28c359912ced8bf05cacd1c864
gaeunnlee <gaeunnnleee@gmail.com>
2026-01-27 22:36:40 +0900

    FLYW-55 feat: 공통 응답 포맷 PageInfo, PageResult 추가 (#21)

--- src/main/java/com/flyway/template/common/ApiResponse.java
+++ src/main/java/com/flyway/template/common/ApiResponse.java
@@ -1,6 +1,6 @@
 package com.flyway.template.common;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,13 +11,29 @@ import lombok.NoArgsConstructor;
  */
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class ApiResponse<T> {
 
 	private boolean success;
 	private T data;
 	private String errorCode;
 	private String message;
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private PageInfo page;
+
+	public ApiResponse(boolean success, T data, String errorCode, String message) {
+		this.success = success;
+		this.data = data;
+		this.errorCode = errorCode;
+		this.message = message;
+	}
+
+	public ApiResponse(boolean success, T data, String errorCode, String message, PageInfo page) {
+		this.success = success;
+		this.data = data;
+		this.errorCode = errorCode;
+		this.message = message;
+		this.page = page;
+	}
 
 	/**
 	 * 성공 응답 (데이터만)
@@ -38,6 +54,17 @@ public class ApiResponse<T> {
 	 */
 	public static <T> ApiResponse<T> success(T data, String message) {
 		return new ApiResponse<>(true, data, null, message);
+	}
+
+	/**
+	 * 성공 응답 (데이터 + 페이지 정보)
+	 * @param data
+	 * @param page
+	 * @return
+	 * @param <T>
+	 */
+	public static <T> ApiResponse<T> success(T data, PageInfo page) {
+		return new ApiResponse<>(true, data, null, null, page);
 	}
 
 	/**
