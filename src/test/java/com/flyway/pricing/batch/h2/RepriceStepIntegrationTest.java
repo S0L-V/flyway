17deed680bae9add20717177290c7cd2bec55efd
cl-o-lc <sujin2839@gmail.com>
2026-01-30 00:39:46 +0900

    FLYW-132 chore: 불필요한 import 제거 (#79)

--- src/test/java/com/flyway/pricing/batch/h2/RepriceStepIntegrationTest.java
+++ src/test/java/com/flyway/pricing/batch/h2/RepriceStepIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.flyway.pricing.batch.h2;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flyway.pricing.service.DynamicPricingCalculator;
 import com.flyway.pricing.batch.reader.RepriceReaderConfig;
 import com.flyway.pricing.batch.writer.RepriceWriterConfig;
@@ -16,8 +15,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.core.*;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
