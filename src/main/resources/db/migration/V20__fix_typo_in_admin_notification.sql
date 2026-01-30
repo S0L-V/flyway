0f6556da0cbce45be3416ae0106b85fea30d172f
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-01-30 15:07:08 +0900

    FLYW-71 fix: flyway 충돌 해결 (#29)

--- src/main/resources/db/migration/V20__fix_typo_in_admin_notification.sql
+++ src/main/resources/db/migration/V20__fix_typo_in_admin_notification.sql
@@ -1,5 +1,3 @@
 ALTER TABLE admin_notification CHANGE COLUMN related_reource_id related_resource_id CHAR(36) NULL;
 
--- bulk_upload_job 테이블 삭제 (FK 제약조건 먼저 삭제)
-ALTER TABLE bulk_upload_job DROP FOREIGN KEY FK_admin_TO_bulk_upload_job_1;
 DROP TABLE IF EXISTS bulk_upload_job;
