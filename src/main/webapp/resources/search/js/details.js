a55c9368830e2abe265443a3e1d6d73331ac768c
김민서 <minseokim0113@gmail.com>
2026-01-31 17:27:36 +0900

    FLYW-181 fix: 월 표시 잘못된거 수정 (#193)

--- src/main/webapp/resources/search/js/details.js
+++ src/main/webapp/resources/search/js/details.js
@@ -140,7 +140,7 @@ function formatDate(dateTime) {
 
     const date = new Date(dateTime);
     const year = String(date.getFullYear());
-    const month = String(date.getMonth()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2,"0");
     return `${year}-${month}-${day}`;
 }
